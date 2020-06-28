52b288e9db1241c707d76b0e897d8a449270f852
Deepak Gunasekaran <deepakgsrn@gmail.com>
2020-06-28 22:16:03 +0530

    Create a new module for user-service microservice

---
+++ apace-user-service/src/main/java/com/de/apace/userservice/BootUserService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.de.apace.userservice;
+
+/**
+ * @author Deepak Gunasekaran
+ * @since  28-Jun-2020 10:02:09 PM
+ *
+ */
+
+public class BootUserService {
+
+	public static void main(String[] args) {
+		
+	}
+}
