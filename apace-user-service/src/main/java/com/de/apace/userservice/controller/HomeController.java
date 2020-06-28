c3fd157318cc6b0cad9439d8d72795f63ca811ed
Deepak Gunasekaran <deepakgsrn@gmail.com>
2020-06-28 22:56:04 +0530

    Change Base URI User Service Module #5

--- apace-user-service/src/main/java/com/de/apace/userservice/controller/HomeController.java
+++ apace-user-service/src/main/java/com/de/apace/userservice/controller/HomeController.java
@@ -1,12 +1,13 @@
 /**
  * 
  */
-package com.de.apace.userservice;
+package com.de.apace.userservice.controller;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping(value = "/api/user")
 public class HomeController {
 
 	@GetMapping("/")
