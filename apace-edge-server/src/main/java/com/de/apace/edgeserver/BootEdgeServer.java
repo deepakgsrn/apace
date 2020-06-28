7524c60664af99f8f865a2a67a93d22ca84befd5
Deepak Gunasekaran <deepakgsrn@gmail.com>
2020-06-28 22:53:31 +0530

    Create a Zuul Gateway #4

---
+++ apace-edge-server/src/main/java/com/de/apace/edgeserver/BootEdgeServer.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.de.apace.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Deepak Gunasekaran
+ * @since 28-Jun-2020 10:44:17 PM
+ *
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+public class BootEdgeServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootEdgeServer.class, args);
+	}
+}
