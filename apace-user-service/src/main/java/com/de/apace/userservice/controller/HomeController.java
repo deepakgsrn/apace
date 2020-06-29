d09a7c8a05c806daf88d7b94c8c8c4f2b2166e9f
Deepak Gunasekaran <deepakgsrn@gmail.com>
2020-06-30 00:20:00 +0530

    added ping controller with server & port details

--- apace-user-service/src/main/java/com/de/apace/userservice/controller/HomeController.java
+++ apace-user-service/src/main/java/com/de/apace/userservice/controller/HomeController.java
@@ -6,6 +6,7 @@ package com.de.apace.userservice.controller;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/user")
 public class HomeController {
-
-	@GetMapping("/")
-	@Produces(value = MediaType.TEXT_XML)
+	
+	
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
+	@Value("${server.port}")
+	private String serverPort;
+	
+	@Value("${spring.profiles.active:default}")
+	private String activeProfile;
+	
+	@GetMapping("/ping")
+	@Produces(value = MediaType.TEXT_PLAIN)
 	public String getHome() {
-		return "Welcome to User Service! ";
+		return "Welcome to User Service! [from "+applicationName+"@"+serverPort+" with active "+activeProfile+" profile ]";
 	}
 }
